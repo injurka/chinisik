95ec805114238c44e11e3a511313f857d66d4228
Ivan Kornilov <ikornilov.ext@prosebya.ru>
2025-04-14 23:19:57 +0400

    yep yep

--- shared/composables/initialize-app.ts
+++ shared/composables/initialize-app.ts
@@ -12,6 +12,11 @@ interface InitializeResult {
 export async function initializeApp(_?: NuxtApp): Promise<InitializeResult> {
   const store = useStore(['auth'])
 
+  const { userAgent } = useDevice()
+
+  if (userAgent === 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36')
+    return { user: null }
+
   try {
     const { data } = await store.auth.me()
     return { user: data }
